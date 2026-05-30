faecfaf37214c94911131b26a67fc581c8e4f34f
nikobellight <nikobellight@gmail.com>
2026-05-30 14:12:12 -0700

    Update test_cookie.py

--- test_cookie.py
+++ test_cookie.py
@@ -4,14 +4,14 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36',
     'Accept': 'application/json, text/javascript, */*; q=0.01',
     'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-    'Origin': 'https://gooutsideandplay.org',
-    'Referer': 'https://gooutsideandplay.org/reservation/camping/index.asp',
+    'Origin': 'https://parksres.sandiegocounty.gov',
+    'Referer': 'https://parksres.sandiegocounty.gov/reservation/camping/index.asp',
     'X-Requested-With': 'XMLHttpRequest'
 }
 
 data = {
-    'parent_idno': '3',
-    'selected_idno': '3',
+    'parent_idno': '9494211',
+    'selected_idno': '9494211',
     'arrive_date': '07/04/2026',
     'depart_date': '07/06/2026',
     'cust_type_idno': '0',
@@ -21,7 +21,7 @@ data = {
 }
 
 response = requests.post(
-    'https://gooutsideandplay.org/reservation/getresults.asp',
+    'https://parksres.sandiegocounty.gov/reservation/getresults.asp',
     headers=headers,
     data=data
 )
