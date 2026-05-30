e3655d9561b67a7d51a3041eb927992473a5761d
nikobellight <nikobellight@gmail.com>
2026-05-29 23:54:04 -0700

    Update test_cookie.py

--- test_cookie.py
+++ test_cookie.py
@@ -1,20 +1,20 @@
 import requests
 
 cookies = {
-    'aws-waf-token': '58342482-7fe9-458b-8dfa-ffa263cde177:FAoAcWstGYkPAAAA:wswIvFPjUmECh3dfs2Y9XNza/w4GNv3JbS2IzAuH3+UCY7V5qefxSG5eg6Rre2gH4428MmEeHss1TiiKPX2R4cfKwtb7sNvvE47SzSkXj88Mzqjj9YIImk4GvST6jC8t14u61fzau1+gSsy+IyUON5c0OuE899O5u5Ry5i4/h+u08uZxDxj14eieXRWcDYc80HuKfX6TjECwTZ3eQoNineqcIunLs/fs5RsfaqGm8HJRU7L1qKdOS88tQF6wQ6bwTYA=',
-    'AWSALB': 'hhXDG37qT5SMN1zCENjHDXUkQ6mkU2AjojZ72JN41uRf3Nho1WJVWluVYkyLPnpHyQ7YeVD8VTOdCQikh5cH3yxrg+RUU37uc1fbksCLxzw4Po2IDgTaUoOIyeiA',
-    'AWSALBCORS': 'hhXDG37qT5SMN1zCENjHDXUkQ6mkU2AjojZ72JN41uRf3Nho1WJVWluVYkyLPnpHyQ7YeVD8VTOdCQikh5cH3yxrg+RUU37uc1fbksCLxzw4Po2IDgTaUoOIyeiA'
+    'aws-waf-token': '58342482-7fe9-458b-8dfa-ffa263cde177:FAoAj3Iut6MbAAAA:UqUzB1C3dlIjbesOFKYimMcXGYyM0gkscs8qZll9cwR2FVCDFk/zfNKZA6gV/gVOwCUBSJrtLsxIhKmpmzKKpUL/tH1aWO9jNsbzBPCK7IVh8ZzJC49qi5WLVSdyBhZ3VG/sWf3Qm1ciO5QNzQh3w0XckNqxlQm3U1cLHU4EEBCkp/lfTi011WePFkaqOUp8gx2vFkxc8YCmJW/Ptuc0RHBhmy+GNS8Py+8Gw3C6FKvtaHBIP5FiNcUEGiqZgIPp+xb0z1xaFP6KlsqIWdw=',
+    'AWSALB': 'Ccq5UCMJcou8fws1TInKH5mnbfxJmo8XUt+iwfeoPliL/lXv5VnODQicjBnWS68T9JFRd5tGropzrokuOfb3EE82TGFDx0u/AAvv7tB/+i7Sznt5dXGrsouSIw/e',
+    'AWSALBCORS': 'Ccq5UCMJcou8fws1TInKH5mnbfxJmo8XUt+iwfeoPliL/lXv5VnODQicjBnWS68T9JFRd5tGropzrokuOfb3EE82TGFDx0u/AAvv7tB/+i7Sznt5dXGrsouSIw/e'
 }
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
     'Accept-Language': 'en-US,en;q=0.5',
-    'Referer': 'https://marin.itinio.com/'
+    'Referer': 'https://secure.sonomacountyparks.org/camp/'
 }
 
 response = requests.get(
-    'https://marin.itinio.com/reservation/camping/index.asp',
+    'https://secure.sonomacountyparks.org/camp/',
     cookies=cookies,
     headers=headers
 )
